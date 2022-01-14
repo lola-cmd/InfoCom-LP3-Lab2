ef97290f0bf443a13abd798cf891836693e43004
Haorui <haorui.peng@eit.lth.se>
2022-01-14 14:57:21 +0100

    move init operation out of main

--- pi/drone.py
+++ pi/drone.py
@@ -3,16 +3,40 @@ from flask_cors import CORS
 import subprocess
 import  requests
 
-from pi.simulator import SERVER_URL
 
 app = Flask(__name__)
 CORS(app, supports_credentials=True)
 app.secret_key = 'dljsaklqk24e21cjn!Ew@@dsa5'
 
+
+myID = "Drone1"
+print(myID)
+init_location = (13.21008, 55.71106)
+try:
+    with open('drone_location.txt', 'r') as file:
+        current_location = file.read()
+        current_longitude = float(current_location.split(',')[0])
+        current_latitude = float(current_location.split(',')[1])
+except FileNotFoundError:
+    current_longitude = init_location[0]
+    current_latitude = init_location[1]
+    with open('drone_location.txt', 'w+') as file:
+        file.write(str(current_longitude) + ',' + str(current_latitude))
+
+drone_info = {'id': myID,
+                'longitude': current_longitude,
+                'latitude': current_latitude,
+                'status': 'idle'}
+print(drone_info)
+
+DATABASE="http://192.168.1.35:5001/drone"
+with requests.Session() as session:
+    resp = session.post(DATABASE, json=drone_info)
+print('done')
+
 @app.route('/', methods=['POST'])
 def main():
     coords = request.json
-    myID = app.config.get('id')
     with open('drone_location.txt', 'r') as file:
         current_location = file.read()
         current_longitude = float(current_location.split(',')[0])
@@ -27,25 +51,4 @@ def main():
     return 'New route received'
 
 if __name__ == '__main__':
-    myID = "Drone1"
-    init_location = (13.21008, 55.71106)
-    try:
-        with open('drone_location.txt', 'r') as file:
-            current_location = file.read()
-            current_longitude = float(current_location.split(',')[0])
-            current_latitude = float(current_location.split(',')[0])
-    except FileNotFoundError:
-        current_longitude = init_location[0]
-        current_latitude = init_location[1]
-        with open('drone_location.txt', 'w+') as file:
-            file.write(str(current_longitude) + ',' + str(current_latitude))
-
-    drone_info = {'id': myID,
-                  'longitude': current_longitude,
-                  'latitude': current_latitude,
-                  'status': 'idle'}
-    DATABASE="http://WEBSERVER_IP:5001/drone"
-    with requests.Session() as session:
-        resp = session.post(DATABASE, json=drone_info)
-    app.config['id'] = myID
     app.run(debug=True, host='0.0.0.0')
