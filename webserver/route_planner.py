fcdc13d83efc1692e39dc47bad1833d1e1158fb3
Robin <karrob@protonmail.ch>
2024-05-15 15:38:56 +0200

    print errors

--- webserver/route_planner.py
+++ webserver/route_planner.py
@@ -26,48 +26,52 @@ def send_request(drone_url, coords):
 
 def planner(from_location, to_location):
     print(from_location, to_location)
-    if to_location is None:
-        message = 'Destination address not found, please input a correct address'
-        return message
-    else:
-        # If the coodinates are found by Nominatim, the coords will need to be sent the a drone that is available
-        coords = { 'from': (from_location["longitude"], from_location["latitude"]), 'to': (to_location["longitude"], to_location["latitude"]), }
-        # if from_location is not None:
-            
-        # else:
-        #     coords = { 'to': (to_location["longitude"], to_location["latitude"]), }
-        # ======================================================================
-        # Here you need to find a drone that is availale from the database. You need to check the status of the drone, there are two status, 'busy' or 'idle', only 'idle' drone is available and can be sent the coords to run delivery
-        # 1. Find avialable drone in the database (Hint: Check keys in RedisServer)
-        # if no drone is availble:
-        message = 'No available drone, try later'
-        
-        all_keys = redis_server.keys()
-        
-        idle_drone = None
-        
-        for key in all_keys:
-            if(key == "phone_position"):
-                continue
 
-            obj = json.loads(redis_server.get(key))
-            print(obj)
-            if obj["status"] == "idle":
-                idle_drone = obj
-                break
-            
-        if idle_drone:
-            # print(idle_drone)
-            # if from_location is None:
-            #     coords["to"] = (float(idle_drone["long"]), float(idle_drone["lat"]))
-            print("COORDS ARE: ", coords)
+    try:
+        if to_location is None:
+            message = 'Destination address not found, please input a correct address'
+            return message
+        else:
+            # If the coodinates are found by Nominatim, the coords will need to be sent the a drone that is available
+            coords = { 'from': (from_location["longitude"], from_location["latitude"]), 'to': (to_location["longitude"], to_location["latitude"]), }
+            # if from_location is not None:
+                
             # else:
-                # 2. Get the IP of available drone, 
-            DRONE_URL = 'http://' + idle_drone["ip"]+':5004'
-                # 3. Send coords to the URL of available drone
-            send_request(DRONE_URL, coords)
-            message = 'Got address and sent request to the drone'
+            #     coords = { 'to': (to_location["longitude"], to_location["latitude"]), }
+            # ======================================================================
+            # Here you need to find a drone that is availale from the database. You need to check the status of the drone, there are two status, 'busy' or 'idle', only 'idle' drone is available and can be sent the coords to run delivery
+            # 1. Find avialable drone in the database (Hint: Check keys in RedisServer)
+            # if no drone is availble:
+            message = 'No available drone, try later'
             
+            all_keys = redis_server.keys()
+            
+            idle_drone = None
+            
+            for key in all_keys:
+                if(key == "phone_position"):
+                    continue
+
+                obj = json.loads(redis_server.get(key))
+                print(obj)
+                if obj["status"] == "idle":
+                    idle_drone = obj
+                    break
+                
+            if idle_drone:
+                # print(idle_drone)
+                # if from_location is None:
+                #     coords["to"] = (float(idle_drone["long"]), float(idle_drone["lat"]))
+                print("COORDS ARE: ", coords)
+                # else:
+                    # 2. Get the IP of available drone, 
+                DRONE_URL = 'http://' + idle_drone["ip"]+':5004'
+                    # 3. Send coords to the URL of available drone
+                send_request(DRONE_URL, coords)
+                message = 'Got address and sent request to the drone'
+    except Exception as e:
+        message = str(e)
+        return message
         
     return message
         # ======================================================================
